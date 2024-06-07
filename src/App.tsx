21ab541a2ad7c6d313abfb846b9f9c2b644af9eb
graphemecluster <graphemecluster@gmail.com>
2024-06-07 17:12:12 +0800

    Move Header to `index.html` to Eliminate `import.meta`; Use Semantic Tag

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ import SentenceCard from "./SentenceCard";
 
 import type { Genre, Language, Sentence } from "./types";
 
-const icon64 = String(new URL("url:../public/assets/favicon-64x64.png", import.meta.url));
-const icon128 = String(new URL("url:../public/assets/favicon-128x128.png", import.meta.url));
-const icon192 = String(new URL("url:../public/assets/favicon-192x192.png", import.meta.url));
-
 export default function App() {
 	const [language, setLanguage] = useState<Language>("waitau");
 	const [genre, setGenre] = useState<Genre>("lit");
@@ -23,20 +19,7 @@ export default function App() {
 		]);
 		setText("");
 	}
-	return <div className="m-auto p-8 max-w-7xl">
-		<div className="grid items-center justify-center grid-cols-1 xs:grid-cols-[auto_1fr_auto] max-sm:max-w-fit">
-			<img className="row-span-2 w-16 mr-4 mb-2 max-xs:hidden" srcSet={`${icon64}, ${icon128} 2x, ${icon192} 3x`} alt="應用程式標誌" />
-			<h1 className="max-sm:col-span-2">香港本土語言文字轉語音朗讀器</h1>
-			<h2 className="my-2">香港本土語言保育協會</h2>
-			<button
-				type="button"
-				className="btn btn-ghost hover:bg-opacity-10
-						   max-sm:text-xl max-sm:font-normal max-sm:relative max-sm:left-4
-						   sm:btn-lg sm:text-[1.375rem] sm:leading-[1.875rem] sm:text-slate-500 sm:col-start-3 sm:row-start-1 sm:row-end-3"
-				onClick={() => (document.getElementById("about-dialog") as HTMLDialogElement).showModal()}>
-				<span className="font-symbol font-normal">🛈</span>關於
-			</button>
-		</div>
+	return <div>
 		<div>
 			<div>
 				<div className="inline-block">
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+document.getElementById("btn-show")!.addEventListener("click", () => {
+	(document.getElementById("about-dialog") as HTMLDialogElement).showModal();
+});
 createRoot(document.getElementById("root")!).render(<App />);
