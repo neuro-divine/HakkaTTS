19be66c920b322574286ad4ad98fbe05db9f4bd1
graphemecluster <graphemecluster@gmail.com>
2024-06-11 00:47:32 +0800

    Fix: Don’t Focus on the Close Button on Dialog Open

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const aboutDialog = document.getElementById("about-dialog") as HTMLDialogElement;
 document.getElementById("btn-show")!.addEventListener("click", () => {
-	(document.getElementById("about-dialog") as HTMLDialogElement).showModal();
+	// https://stackoverflow.com/a/76158858
+	aboutDialog.inert = true;
+	aboutDialog.showModal();
+	aboutDialog.inert = false;
 });
+
 createRoot(document.getElementById("root")!).render(<App />);
