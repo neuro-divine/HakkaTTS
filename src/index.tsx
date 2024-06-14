1f39ce3642827724904a9795bc72d2dec9ce67d8
graphemecluster <graphemecluster@gmail.com>
2024-06-15 02:50:01 +0800

    While virtual keyboard is visible on mobiles, disable pull to refresh & attempt to correct container height to improve scrolling behavior

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+if (visualViewport !== null) {
+	// https://github.com/w3c/csswg-drafts/issues/7194, https://github.com/w3c/csswg-drafts/issues/7475
+	const container = document.body.firstElementChild as HTMLDivElement;
+	let prevWidth = visualViewport.width;
+	let prevHeight = visualViewport.height;
+	visualViewport.addEventListener("resize", () => {
+		const currWidth = visualViewport!.width;
+		const currHeight = visualViewport!.height;
+		container.style.transition = prevWidth === (prevWidth = currWidth) && prevHeight > (prevHeight = currHeight)
+			? "height 800ms cubic-bezier(0.2, 0.8, 0.4, 1)"
+			: "";
+		container.style.height = `${currHeight}px`;
+	});
+}
+
 const aboutDialog = document.getElementById("about-dialog") as HTMLDialogElement;
 document.getElementById("btn-show")!.addEventListener("click", () => {
 	// https://stackoverflow.com/a/76158858
